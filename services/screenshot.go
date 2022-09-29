27e44f8420beda82eddf5d7823d658c42ee5141c
russell <russell.chen@bimap.co>
2022-09-29 19:05:57 +0800

    update

--- services/screenshot.go
+++ services/screenshot.go
@@ -16,34 +16,35 @@ import (
 	// "github.com/chromedp/cdproto/page"
 )
 
-
 func ScreenshotDocker(ReportID int) {
 
-	ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.120:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
-	// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://127.0.0.1:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
-	// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.138:9222/devtools/page")
-	defer cancel()
+	////// -------------------------
+	// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.120:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
+	// // ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://127.0.0.1:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
+	// // ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.138:9222/devtools/page")
+	// defer cancel()
 
-	ctx, cancel = chromedp.NewContext(ctx,
-		// chromedp.WithDebugf(log.Printf),
-	)
-	defer cancel()
+	// ctx, cancel = chromedp.NewContext(ctx,
+	// 	// chromedp.WithDebugf(log.Printf),
+	// )
+	// defer cancel()
 
-	// go get the current targets in the Electron app.
-	ts, err := chromedp.Targets(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // go get the current targets in the Electron app.
+	// ts, err := chromedp.Targets(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	if len(ts) == 0 {
-		log.Fatal("targets not found.")
-	}
+	// if len(ts) == 0 {
+	// 	log.Fatal("targets not found.")
+	// }
 
-	ctx, cancel = chromedp.NewContext(ctx,
-		// use the first target
-		chromedp.WithTargetID(ts[0].TargetID),
-	)
+	// ctx, cancel = chromedp.NewContext(ctx,
+	// 	// use the first target
+	// 	chromedp.WithTargetID(ts[0].TargetID),
+	// )
 
+	////// -------------------------
 	// // create context
 	// ctx, cancel := chromedp.NewContext(
 	// 	context.Background(),
@@ -52,26 +53,49 @@ func ScreenshotDocker(ReportID int) {
 	// defer cancel()
 
 	// 先取得 report 中包含的 instance
-	data,err := GetInstanceInReportbyReportID(ReportID)
+	data, err := GetInstanceInReportbyReportID(ReportID)
 	if err != nil {
 		fmt.Println(err)
 	}
-	reportinfo,err := GetReportByReportID(ReportID)
+	reportinfo, err := GetReportByReportID(ReportID)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	//判斷 report 中 instance 的 type 是 kibana or grafana
-	for _,data1 := range data{
+	for _, data1 := range data {
 		// Kibana 的區塊
 		if data1.Type == "kibana" {
 			// 取得屬於此 instance 的 dashboards
-			dashboard,err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
+			dashboard, err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println("remote browser,instance ID:",data1.InstanceID)
-			for _,data2 := range dashboard {
+			fmt.Println("remote browser,instance ID:", data1.InstanceID)
+			for _, data2 := range dashboard {
+				ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.120:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
+				// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://127.0.0.1:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
+				// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.138:9222/devtools/page")
+				defer cancel()
+
+				ctx, cancel = chromedp.NewContext(ctx) // chromedp.WithDebugf(log.Printf),
+
+				defer cancel()
+
+				// go get the current targets in the Electron app.
+				ts, err := chromedp.Targets(ctx)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if len(ts) == 0 {
+					log.Fatal("targets not found.")
+				}
+
+				ctx, cancel = chromedp.NewContext(ctx,
+					// use the first target
+					chromedp.WithTargetID(ts[0].TargetID),
+				)
 				// create context 截圖程式碼，擺著就好勿動
 				// ctx, cancel := chromedp.NewContext(
 				// 	context.Background(),
@@ -79,32 +103,56 @@ func ScreenshotDocker(ReportID int) {
 				// )
 				// defer cancel()
 				// 用 report 中 instance 的 instance ID 去 instance 資料庫中撈出 instance 的 IP,type,user,password
-				auth,err := GetInstanceTypeAndIP(data1.InstanceID)
+				auth, err := GetInstanceTypeAndIP(data1.InstanceID)
 				if err != nil {
 					fmt.Println(err)
 				}
 				// capture screenshot of an element 截圖程式碼，擺著就好勿動
 				var buf []byte
 				// 將取出來的個參數帶入網址中以便截圖
-				url := fmt.Sprintf("%s/app/dashboards#/view/%s?_g=(time:(from:%s,to:%s))&_a=(fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:lucene,query:''),tags:!(),timeRestore:!t,viewMode:view)",auth["IP"],data2.UID,reportinfo.From,reportinfo.To)
+				url := fmt.Sprintf("%s/app/dashboards#/view/%s?_g=(time:(from:%s,to:%s))&_a=(fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:lucene,query:''),tags:!(),timeRestore:!t,viewMode:view)", auth["IP"], data2.UID, reportinfo.From, reportinfo.To)
 				// 帶入 url,instance 的 username 、 password 帶入截圖的 function
-				if err := chromedp.Run(ctx,kibanaElementScreenshotWithAuth(url, auth["user"],auth["password"],`div.dashboardViewport`,&buf),network.ClearBrowserCookies()); err != nil {
+				if err := chromedp.Run(ctx, kibanaElementScreenshotWithAuth(url, auth["user"], auth["password"], `div.dashboardViewport`, &buf), network.ClearBrowserCookies()); err != nil {
 					log.Fatal(err)
 				}
 				// 寫到指定路徑，暫定以 dashboard UID 命名
-				file := fmt.Sprintf("%s/%s.png",global.EnvConfig.Reportengine.PicturePath,data2.UID)
+				file := fmt.Sprintf("%s/%s.png", global.EnvConfig.Reportengine.PicturePath, data2.UID)
 				if err := ioutil.WriteFile(file, buf, 0o644); err != nil {
 					log.Fatal(err)
 				}
-				fmt.Println("remote browser,dashboard download success:",data2.UID)
-			}	
-		}else if data1.Type == "grafana" {
-			dashboard,err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
+				defer chromedp.Cancel(ctx)
+				fmt.Println("remote browser,dashboard download success:", data2.UID)
+			}
+		} else if data1.Type == "grafana" {
+			dashboard, err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println("remote browser,instance ID:",data1.InstanceID)
-			for _,data2 := range dashboard {
+			fmt.Println("remote browser,instance ID:", data1.InstanceID)
+			for _, data2 := range dashboard {
+				ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.120:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
+				// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://127.0.0.1:3003/devtools/page/BROWSERLESSGDV8E5H4386JYCYGVRBAQ")
+				// ctx, cancel := chromedp.NewRemoteAllocator(context.Background(), "ws://10.99.1.138:9222/devtools/page")
+				defer cancel()
+
+				ctx, cancel = chromedp.NewContext(ctx) // chromedp.WithDebugf(log.Printf),
+
+				defer cancel()
+
+				// go get the current targets in the Electron app.
+				ts, err := chromedp.Targets(ctx)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if len(ts) == 0 {
+					log.Fatal("targets not found.")
+				}
+
+				ctx, cancel = chromedp.NewContext(ctx,
+					// use the first target
+					chromedp.WithTargetID(ts[0].TargetID),
+				)
 				fmt.Println(data2)
 				// create context 截圖程式碼，擺著就好勿動
 				// ctx, cancel := chromedp.NewContext(
@@ -113,35 +161,33 @@ func ScreenshotDocker(ReportID int) {
 				// )
 				// defer cancel()
 				// 用 report 中 instance 的 instance ID 去 instance 資料庫中撈出 instance 的 IP,type,user,password
-				auth,err := GetInstanceTypeAndIP(data1.InstanceID)
+				auth, err := GetInstanceTypeAndIP(data1.InstanceID)
 				if err != nil {
 					fmt.Println(err)
 				}
 				// capture screenshot of an element 截圖程式碼，擺著就好勿動
 				var buf []byte
 				// 將取出來的個參數帶入網址中以便截圖
-				url := fmt.Sprintf("%s/grafana_iframe/d/%s/?from=%s/d&to=%s&orgId=1",auth["IP"],data2.UID,reportinfo.From,reportinfo.To)
+				url := fmt.Sprintf("%s/grafana_iframe/d/%s/?from=%s/d&to=%s&orgId=1", auth["IP"], data2.UID, reportinfo.From, reportinfo.To)
 				// 帶入 url,instance 的 username 、 password 帶入截圖的 function
-				if err := chromedp.Run(ctx,grafanaElementScreenshotWithAuth(url, auth["user"],auth["password"],`div.scrollbar-view`,&buf),network.ClearBrowserCookies()); err != nil {
+				if err := chromedp.Run(ctx, grafanaElementScreenshotWithAuth(url, auth["user"], auth["password"], `div.scrollbar-view`, &buf), network.ClearBrowserCookies()); err != nil {
 					log.Fatal(err)
 				}
 				// 寫到指定路徑，暫定以 dashboard UID 命名
-				file := fmt.Sprintf("%s/%s.png",global.EnvConfig.Reportengine.PicturePath,data2.UID)
+				file := fmt.Sprintf("%s/%s.png", global.EnvConfig.Reportengine.PicturePath, data2.UID)
 				if err := ioutil.WriteFile(file, buf, 0o644); err != nil {
 					log.Fatal(err)
 				}
-				fmt.Println("remote browser,dashboard download success:",data2.UID)
-				
+				defer chromedp.Cancel(ctx)
+				fmt.Println("remote browser,dashboard download success:", data2.UID)
+
 			}
-			
+
 		}
 
 	}
-	
+
 }
-
-
-
 
 func Screenshot(ReportID int) {
 
@@ -153,27 +199,27 @@ func Screenshot(ReportID int) {
 	// defer cancel()
 
 	// 先取得 report 中包含的 instance
-	data,err := GetInstanceInReportbyReportID(ReportID)
+	data, err := GetInstanceInReportbyReportID(ReportID)
 	if err != nil {
 		fmt.Println(err)
 	}
-	reportinfo,err := GetReportByReportID(ReportID)
+	reportinfo, err := GetReportByReportID(ReportID)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	//判斷 report 中 instance 的 type 是 kibana or grafana
-	for _,data1 := range data{
+	for _, data1 := range data {
 		// Kibana 的區塊
 		if data1.Type == "kibana" {
 			// 取得屬於此 instance 的 dashboards
-			dashboard,err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
+			dashboard, err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println("instance id :",data1.InstanceID)
-			
-			for _,data2 := range dashboard {
+			fmt.Println("instance id :", data1.InstanceID)
+
+			for _, data2 := range dashboard {
 				// create context 截圖程式碼，擺著就好勿動
 				ctx, cancel := chromedp.NewContext(
 					context.Background(),
@@ -181,31 +227,31 @@ func Screenshot(ReportID int) {
 				)
 				defer cancel()
 				// 用 report 中 instance 的 instance ID 去 instance 資料庫中撈出 instance 的 IP,type,user,password
-				auth,err := GetInstanceTypeAndIP(data1.InstanceID)
+				auth, err := GetInstanceTypeAndIP(data1.InstanceID)
 				if err != nil {
 					fmt.Println(err)
 				}
 				// capture screenshot of an element 截圖程式碼，擺著就好勿動
 				var buf []byte
 				// 將取出來的個參數帶入網址中以便截圖
-				url := fmt.Sprintf("%s/app/dashboards#/view/%s?_g=(time:(from:%s,to:%s))&_a=(fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:lucene,query:''),tags:!(),timeRestore:!t,viewMode:view)",auth["IP"],data2.UID,reportinfo.From,reportinfo.To)
+				url := fmt.Sprintf("%s/app/dashboards#/view/%s?_g=(time:(from:%s,to:%s))&_a=(fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:lucene,query:''),tags:!(),timeRestore:!t,viewMode:view)", auth["IP"], data2.UID, reportinfo.From, reportinfo.To)
 				// 帶入 url,instance 的 username 、 password 帶入截圖的 function
-				if err := chromedp.Run(ctx,kibanaElementScreenshotWithAuth(url, auth["user"],auth["password"],`div.dashboardViewport`,&buf)); err != nil {
+				if err := chromedp.Run(ctx, kibanaElementScreenshotWithAuth(url, auth["user"], auth["password"], `div.dashboardViewport`, &buf)); err != nil {
 					log.Fatal(err)
 				}
 				// 寫到指定路徑，暫定以 dashboard UID 命名
-				file := fmt.Sprintf("%s/%s.png",global.EnvConfig.Reportengine.PicturePath,data2.UID)
+				file := fmt.Sprintf("%s/%s.png", global.EnvConfig.Reportengine.PicturePath, data2.UID)
 				if err := ioutil.WriteFile(file, buf, 0o644); err != nil {
 					log.Fatal(err)
 				}
-			}	
-		}else if data1.Type == "grafana" {
-			dashboard,err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
+			}
+		} else if data1.Type == "grafana" {
+			dashboard, err := GetDashboardOfInstanceByInstanceID(data1.InstanceID)
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println("instance id :",data1.InstanceID)
-			for _,data2 := range dashboard {
+			fmt.Println("instance id :", data1.InstanceID)
+			for _, data2 := range dashboard {
 				// create context 截圖程式碼，擺著就好勿動
 				ctx, cancel := chromedp.NewContext(
 					context.Background(),
@@ -213,49 +259,45 @@ func Screenshot(ReportID int) {
 				)
 				defer cancel()
 				// 用 report 中 instance 的 instance ID 去 instance 資料庫中撈出 instance 的 IP,type,user,password
-				auth,err := GetInstanceTypeAndIP(data1.InstanceID)
+				auth, err := GetInstanceTypeAndIP(data1.InstanceID)
 				if err != nil {
 					fmt.Println(err)
 				}
 				// capture screenshot of an element 截圖程式碼，擺著就好勿動
 				var buf []byte
 				// 將取出來的個參數帶入網址中以便截圖
-				url := fmt.Sprintf("%s/grafana_iframe/d/%s/?from=%s/d&to=%s&orgId=1",auth["IP"],data2.UID,reportinfo.From,reportinfo.To)
+				url := fmt.Sprintf("%s/grafana_iframe/d/%s/?from=%s/d&to=%s&orgId=1", auth["IP"], data2.UID, reportinfo.From, reportinfo.To)
 				// 帶入 url,instance 的 username 、 password 帶入截圖的 function
-				if err := chromedp.Run(ctx,grafanaElementScreenshotWithAuth(url, auth["user"],auth["password"],`div.scrollbar-view`,&buf)); err != nil {
+				if err := chromedp.Run(ctx, grafanaElementScreenshotWithAuth(url, auth["user"], auth["password"], `div.scrollbar-view`, &buf)); err != nil {
 					log.Fatal(err)
 				}
 				// 寫到指定路徑，暫定以 dashboard UID 命名
-				file := fmt.Sprintf("%s/%s.png",global.EnvConfig.Reportengine.PicturePath,data2.UID)
+				file := fmt.Sprintf("%s/%s.png", global.EnvConfig.Reportengine.PicturePath, data2.UID)
 				if err := ioutil.WriteFile(file, buf, 0o644); err != nil {
 					log.Fatal(err)
 				}
-				
+
 			}
-			
+
 		}
 
 	}
-	
+
 }
 
-
-
-
 // 查 instance 的資訊
-func GetInstanceInfoByInstanceID(InstanceID int) (entities.Instance, error){
+func GetInstanceInfoByInstanceID(InstanceID int) (entities.Instance, error) {
 
 	var instance entities.Instance
 	instance.InstanceID = InstanceID
 
-	err := global.Mysql.Where("instance_id= ?",InstanceID).First(&instance).Error
+	err := global.Mysql.Where("instance_id= ?", InstanceID).First(&instance).Error
 	if err != nil {
 		return instance, err
 	}
 	return instance, nil
 
 }
-
 
 // // 查單一Instance
 // func GetInstanceByID(instanceID int) (entities.Instance, error) {
@@ -269,11 +311,10 @@ func GetInstanceInfoByInstanceID(InstanceID int) (entities.Instance, error){
 // 	return instance, nil
 // }
 
-
 // 只取出 Instance 中需要的元素
-func GetInstanceTypeAndIP(id int) (map[string]string, error){
+func GetInstanceTypeAndIP(id int) (map[string]string, error) {
 	// var IPType []map[string]string
-	data,err := GetInstanceByID(id)
+	data, err := GetInstanceByID(id)
 	if err != nil {
 		fmt.Println(err)
 		// log.Logrecord("ERROR",err.Error())
@@ -285,8 +326,9 @@ func GetInstanceTypeAndIP(id int) (map[string]string, error){
 	IPType["type"] = data.Type
 	IPType["user"] = data.User
 	IPType["password"] = data.Pass
-	return IPType,err
+	return IPType, err
 }
+
 // 查 Dashboard uid by report ID
 func GetDashboardByReportID(reportID int) (entities.Report, error) {
 
@@ -311,22 +353,22 @@ func GetAllDashboard() ([]entities.RDashboard, error) {
 	return instancies, nil
 }
 
-func GetUID() ([]string,error){
-	uid,err := GetAllDashboard()
+func GetUID() ([]string, error) {
+	uid, err := GetAllDashboard()
 	if err != nil {
 		fmt.Println(err)
 		// log.Logrecord("ERROR",err.Error())
 		//return
 	}
 	var UID1 []string
-	for _,uids := range uid {
+	for _, uids := range uid {
 		// var UID1 []string
-		UID1 = append(UID1,uids.UID)
-	}	
+		UID1 = append(UID1, uids.UID)
+	}
 	fmt.Println(UID1)
 	if err != nil {
 		return nil, err
 	}
-	return UID1,nil
+	return UID1, nil
 
 }
