d4bf865e3bf49627e34f61e826becbc80caba5df
russell <russell.chen@bimap.co>
2022-09-30 16:42:35 +0800

    schedule update

--- entities/schedule.go
+++ entities/schedule.go
@@ -2,16 +2,15 @@ package entities
 
 type Schedule struct {
 	Common
-	ScheduleID     int    `json:"schedule_id" form:"schedule_id" gorm:"primaryKey"`
-	Name           string `json:"schedule_name" form:"schedule_name"`
-	Description    string `json:"description" form:"description"`
+	ScheduleID  int    `json:"schedule_id" form:"schedule_id" gorm:"primaryKey"`
+	Name        string `json:"schedule_name" form:"schedule_name"`
+	Description string `json:"description" form:"description"`
 	// Report         string `json:"reports" form:"reports"`
-	GenerateReport string `json:"generate-report" form:"generate_report"`
-	SendReport     string `json:"send_report" form:"send_report"`
-	Recipient      string `json:"recipients" form:"recipients"`
-	Status         string `json:"status" form:"status"`
-	Report []Report `gorm:"foreignKey:ScheduleID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-
+	GenerateReport string   `json:"generate-report" form:"generate_report"`
+	SendReport     string   `json:"send_report" form:"send_report"`
+	Recipient      string   `json:"recipients" form:"recipients"`
+	Status         string   `json:"status" form:"status"`
+	Report         []Report `json:"report" form:"report" gorm:"-"`
 }
 
 type Status struct {
