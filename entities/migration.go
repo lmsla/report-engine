3ff6cf0b528b3d1292c78eeee2aec5d7f55a8aab
russell <russell.chen@bimap.co>
2022-09-30 17:39:52 +0800

    schedule update

--- entities/migration.go
+++ entities/migration.go
@@ -53,52 +53,52 @@ func InitTable() {
 		os.Exit(0)
 	}
 
-	err = global.Mysql.Omit("Roles.Dashboards").Create(&UserData).Error
-	if err != nil {
-		color.Warn.Printf("[Mysql]-->初始化User數據表失敗,err: %v\n", err)
-		os.Exit(0)
-	}
+	// err = global.Mysql.Omit("Roles.Dashboards").Create(&UserData).Error
+	// if err != nil {
+	// 	color.Warn.Printf("[Mysql]-->初始化User數據表失敗,err: %v\n", err)
+	// 	os.Exit(0)
+	// }
 
-	err = global.Mysql.Create(&InstanceData).Error
-	if err != nil {
-		color.Warn.Printf("[Mysql]-->初始化Inventory數據表失敗,err: %v\n", err)
-		os.Exit(0)
-	}
+	// err = global.Mysql.Create(&InstanceData).Error
+	// if err != nil {
+	// 	color.Warn.Printf("[Mysql]-->初始化Inventory數據表失敗,err: %v\n", err)
+	// 	os.Exit(0)
+	// }
 
-	// 寫入Relation Table (dashboards_roles)
-	for i := 0; i < len(UserData); i++ {
-		for j := 0; j < len(UserData[i].Roles); j++ {
-			for k := 0; k < len(UserData[i].Roles[j].Dashboards); k++ {
-				var dashboardID, roleID int
-				global.Mysql.Table("dashboards").Where("dashboard_name = ?", UserData[i].Roles[j].Dashboards[k].DashboardName).Select("dashboard_id").Scan(&dashboardID)
-				global.Mysql.Table("roles").Where("role_name = ?", UserData[i].Roles[j].RoleName).Select("role_id").Scan(&roleID)
-				err := global.Mysql.Table("dashboards_roles").Create(map[string]interface{}{"dashboard_id": dashboardID, "role_id": roleID}).Error
-				if err != nil {
-					color.Warn.Printf("[Mysql]-->初始化dashboard_roles數據表失敗,err: %v\n", err)
-					os.Exit(0)
-				}
-			}
+	// // 寫入Relation Table (dashboards_roles)
+	// for i := 0; i < len(UserData); i++ {
+	// 	for j := 0; j < len(UserData[i].Roles); j++ {
+	// 		for k := 0; k < len(UserData[i].Roles[j].Dashboards); k++ {
+	// 			var dashboardID, roleID int
+	// 			global.Mysql.Table("dashboards").Where("dashboard_name = ?", UserData[i].Roles[j].Dashboards[k].DashboardName).Select("dashboard_id").Scan(&dashboardID)
+	// 			global.Mysql.Table("roles").Where("role_name = ?", UserData[i].Roles[j].RoleName).Select("role_id").Scan(&roleID)
+	// 			err := global.Mysql.Table("dashboards_roles").Create(map[string]interface{}{"dashboard_id": dashboardID, "role_id": roleID}).Error
+	// 			if err != nil {
+	// 				color.Warn.Printf("[Mysql]-->初始化dashboard_roles數據表失敗,err: %v\n", err)
+	// 				os.Exit(0)
+	// 			}
+	// 		}
 
-		}
-	}
+	// 	}
+	// }
 
-	err = global.Mysql.Omit("Dashboards").Create(&MenuData).Error
-	if err != nil {
-		color.Warn.Printf("[Mysql]-->初始化Menu數據表失敗,err: %v\n", err)
-		os.Exit(0)
-	}
+	// err = global.Mysql.Omit("Dashboards").Create(&MenuData).Error
+	// if err != nil {
+	// 	color.Warn.Printf("[Mysql]-->初始化Menu數據表失敗,err: %v\n", err)
+	// 	os.Exit(0)
+	// }
 
-	for i := 0; i < len(MenuData.Dashboards); i++ {
-		var menuID int
-		global.Mysql.Table("menus").Where("menu_name = ?", MenuData.MenuName).Select("menu_id").Scan(&menuID)
-		//global.Mysql.Table("dashboards").Where("dashboard_name = ?", MenuData.Dashboards[i].DashboardName).Select("dashboard_id").Scan(&dashboardID)
-		//err := global.Mysql.Table("menus_dashboards").Create(map[string]interface{}{"dashboard_id": dashboardID, "menu_id": menuID}).Error
-		err := global.Mysql.Table("dashboards").Where("dashboard_name = ?", MenuData.Dashboards[i].DashboardName).Update("menu_id", menuID).Error
-		if err != nil {
-			color.Warn.Printf("[Mysql]-->初始化menus_dashboards數據表失敗,err: %v\n", err)
-			os.Exit(0)
-		}
-	}
+	// for i := 0; i < len(MenuData.Dashboards); i++ {
+	// 	var menuID int
+	// 	global.Mysql.Table("menus").Where("menu_name = ?", MenuData.MenuName).Select("menu_id").Scan(&menuID)
+	// 	//global.Mysql.Table("dashboards").Where("dashboard_name = ?", MenuData.Dashboards[i].DashboardName).Select("dashboard_id").Scan(&dashboardID)
+	// 	//err := global.Mysql.Table("menus_dashboards").Create(map[string]interface{}{"dashboard_id": dashboardID, "menu_id": menuID}).Error
+	// 	err := global.Mysql.Table("dashboards").Where("dashboard_name = ?", MenuData.Dashboards[i].DashboardName).Update("menu_id", menuID).Error
+	// 	if err != nil {
+	// 		color.Warn.Printf("[Mysql]-->初始化menus_dashboards數據表失敗,err: %v\n", err)
+	// 		os.Exit(0)
+	// 	}
+	// }
 
 	global.Mysql.Migrator().CreateConstraint(&Instance{}, "Dashboards")
 	global.Mysql.Migrator().CreateConstraint(&User{}, "Roles")
@@ -113,49 +113,49 @@ func InitTable() {
 	color.Info.Println("[Mysql]-->初始化數據成功")
 }
 
-var UserData = []User{
-	{
-		UserName: "jessie",
-		Roles: []Role{
-			{
-				RoleName: "admin",
-				Dashboards: []Dashboard{
-					{
-						DashboardName: "Single Node Analysis - cpu",
-					},
-					{
-						DashboardName: "Single Node Analysis - mem",
-					},
-				},
-			},
-		},
-	},
-}
+// var UserData = []User{
+// 	{
+// 		UserName: "jessie",
+// 		Roles: []Role{
+// 			{
+// 				RoleName: "admin",
+// 				Dashboards: []Dashboard{
+// 					{
+// 						DashboardName: "Single Node Analysis - cpu",
+// 					},
+// 					{
+// 						DashboardName: "Single Node Analysis - mem",
+// 					},
+// 				},
+// 			},
+// 		},
+// 	},
+// }
 
-var InstanceData = []Instance{
-	{
-		Auth:         false,
-		IP:           "http://10.99.1.240:3000",
-		InstanceName: "grafana-240",
-		Type:         "grafana",
-		User:         "admin",
-		Pass:         "12345678",
-		Dashboards: []Dashboard{
-			{
-				DashboardName: "Single Node Analysis - cpu",
-				Alias:         "Demo1",
-				Description:   "cpu",
-				UID:           "mOTd6X67k",
-			},
-			{
-				DashboardName: "Single Node Analysis - mem",
-				Alias:         "Demo2",
-				Description:   "mem",
-				UID:           "RlgkWFg4k",
-			},
-		},
-	},
-}
+// var InstanceData = []Instance{
+// 	{
+// 		Auth:         false,
+// 		IP:           "http://10.99.1.240:3000",
+// 		InstanceName: "grafana-240",
+// 		Type:         "grafana",
+// 		User:         "admin",
+// 		Pass:         "12345678",
+// 		Dashboards: []Dashboard{
+// 			{
+// 				DashboardName: "Single Node Analysis - cpu",
+// 				Alias:         "Demo1",
+// 				Description:   "cpu",
+// 				UID:           "mOTd6X67k",
+// 			},
+// 			{
+// 				DashboardName: "Single Node Analysis - mem",
+// 				Alias:         "Demo2",
+// 				Description:   "mem",
+// 				UID:           "RlgkWFg4k",
+// 			},
+// 		},
+// 	},
+// }
 
 var MenuData = Menu{
 	MenuName: "Single Node",
