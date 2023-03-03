17024b6082dfdaa5d688406dbd8f25a95b229118
russell <russell.chen@bimap.co>
2023-03-03 14:09:05 +0800

    update host

--- docs/docs.go
+++ docs/docs.go
@@ -1191,7 +1191,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8005",
+	Host:             "10.99.1.127:8005",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http"},
 	Title:            "Report Engine Golang API",
