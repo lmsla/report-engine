18d4714c7fbcec48f16a48fd82902fa96db39106
russell <russell.chen@bimap.co>
2023-05-17 15:37:21 +0800

    dropdown adjust

--- controller/dropdown.go
+++ controller/dropdown.go
@@ -2,10 +2,10 @@ package controller
 
 import (
 	"net/http"
-	// "strconv"
+	"strconv"
 
 	// "report-backend-golang/models"
-	"report-backend-golang/entities"
+	// "report-backend-golang/entities"
 	"report-backend-golang/handler"
 	"report-backend-golang/services"
 
@@ -25,14 +25,32 @@ import (
 // @Router /Dropdown [get]
 func GetDropdownSource(c *gin.Context) {
 
-	body := new(entities.DropdownBody)
-	c.Bind(&body)
+	source_type := c.Param("source_type")
+	if err := c.Error; err != nil {
+		c.JSON(http.StatusBadRequest, c.Error)
+		return
+	}
+
+	space_name := c.Param("space_name")
+	if err := c.Error; err != nil {
+		c.JSON(http.StatusBadRequest, c.Error)
+		return
+	}
+
+	InstanceID, err := strconv.Atoi(c.Param("instance_id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+
+
 	// if err := services.GetDropdownSource(body); err != nil {
 
 	// }
-	switch body.SourceType {
+	switch source_type {
 	case "space":
-		inventory, err := services.GetInstanceByID(body.InstanceID)
+		inventory, err := services.GetInstanceByID(InstanceID)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, "error to get inventory details")
 			handler.WriteErrorLog(c, "error to get inventory details")
@@ -48,13 +66,13 @@ func GetDropdownSource(c *gin.Context) {
 		c.JSON(http.StatusOK, spaces)
 
 	case "dashboard":
-		inventory, err := services.GetInstanceByID(body.InstanceID)
+		inventory, err := services.GetInstanceByID(InstanceID)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, "error to get inventory details")
 			handler.WriteErrorLog(c, "error to get inventory details")
 			return
 		}
-		dashboarddata, err := services.GetALLKibanaDashboardTitle1(body.SpaceName, inventory)
+		dashboarddata, err := services.GetALLKibanaDashboardTitle1(space_name, inventory)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, "error to get dashboard details")
 			handler.WriteErrorLog(c, "error to get dashboard details")
@@ -63,13 +81,13 @@ func GetDropdownSource(c *gin.Context) {
 		c.JSON(http.StatusOK, dashboarddata)
 
 	case "visualization":
-		inventory, err := services.GetInstanceByID(body.InstanceID)
+		inventory, err := services.GetInstanceByID(InstanceID)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, "error to get inventory details")
 			handler.WriteErrorLog(c, "error to get inventory details")
 			return
 		}
-		visualdata, err := services.GetALLKibanaVisualizationTitle1(body.SpaceName, inventory)
+		visualdata, err := services.GetALLKibanaVisualizationTitle1(space_name, inventory)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, "error to get visualization details")
 			handler.WriteErrorLog(c, "error to get visualization details")
@@ -78,5 +96,57 @@ func GetDropdownSource(c *gin.Context) {
 		c.JSON(http.StatusOK, visualdata)
 
 	}
+
+	// body := new(entities.DropdownBody)
+	// c.Bind(&body)
+
+	// switch body.SourceType {
+	// case "space":
+	// 	inventory, err := services.GetInstanceByID(body.InstanceID)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, "error to get inventory details")
+	// 		handler.WriteErrorLog(c, "error to get inventory details")
+	// 		return
+	// 	}
+	// 	spaces, err := services.GetKibanaSpaces1(inventory)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, "error to get space details")
+	// 		handler.WriteErrorLog(c, "error to get space details")
+	// 		return
+	// 	}
+	// 	// fmt.Println(spaces)
+	// 	c.JSON(http.StatusOK, spaces)
+
+	// case "dashboard":
+	// 	inventory, err := services.GetInstanceByID(body.InstanceID)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, "error to get inventory details")
+	// 		handler.WriteErrorLog(c, "error to get inventory details")
+	// 		return
+	// 	}
+	// 	dashboarddata, err := services.GetALLKibanaDashboardTitle1(body.SpaceName, inventory)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, "error to get dashboard details")
+	// 		handler.WriteErrorLog(c, "error to get dashboard details")
+	// 		return
+	// 	}
+	// 	c.JSON(http.StatusOK, dashboarddata)
+
+	// case "visualization":
+	// 	inventory, err := services.GetInstanceByID(body.InstanceID)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, "error to get inventory details")
+	// 		handler.WriteErrorLog(c, "error to get inventory details")
+	// 		return
+	// 	}
+	// 	visualdata, err := services.GetALLKibanaVisualizationTitle1(body.SpaceName, inventory)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, "error to get visualization details")
+	// 		handler.WriteErrorLog(c, "error to get visualization details")
+	// 		return
+	// 	}
+	// 	c.JSON(http.StatusOK, visualdata)
+
+	// }
 
 }
