12aa79e07e52dd79068ea822d76eb47d15d26fe0
russell <russell.chen@bimap.co>
2024-02-21 12:38:23 +0800

    update

--- db/migrations/000005_create_schedule_table.up.sql
+++ db/migrations/000005_create_schedule_table.up.sql
@@ -5,10 +5,14 @@ CREATE TABLE `schedules` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `cron_time` varchar(50) DEFAULT NULL,
-  `to` varchar(255) DEFAULT NULL,
-  `cc` varchar(255) DEFAULT NULL,
-  `bcc` varchar(255) DEFAULT NULL,
+  `to` text,
+  `cc` text,
+  `bcc` text,
   `cron_id` bigint DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `body` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_schedules_id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
--- db/migrations/000006_create_hitsory_table.up.sql
+++ db/migrations/000006_create_hitsory_table.up.sql
@@ -2,14 +2,16 @@ CREATE TABLE `histories` (
   `created_at` bigint unsigned DEFAULT NULL,
   `updated_at` bigint unsigned DEFAULT NULL,
   `deleted_at` bigint DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `schedule_id` bigint DEFAULT NULL,
   `schedule_name` varchar(50) DEFAULT NULL,
-  `to` varchar(50) DEFAULT NULL,
-  `cc` varchar(50) DEFAULT NULL,
-  `bcc` varchar(50) DEFAULT NULL,
+  `to` text,
+  `cc` text,
+  `bcc` text,
   `execute_time` bigint unsigned DEFAULT NULL,
   `email_time` bigint unsigned DEFAULT NULL,
   `success` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `fk_histories_schedule` (`schedule_id`),
   CONSTRAINT `fk_histories_schedule` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=395 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
