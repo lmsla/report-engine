b752eca169f7754e1b8bf1cc9af19e7560d32611
russell <russell.chen@bimap.co>
2024-08-14 11:44:24 +0800

    upadate CreateHistoryReport

--- router/router.go
+++ router/router.go
@@ -78,7 +78,9 @@ func LoadRouter() *gin.Engine {
 		apiv1_auth.GET("/History/GetOldHistory", GetOldHitory)
 		apiv1_auth.GET("/History/GetHistory/:id", GetHistoryByHistoryID)
 		// apiv1_auth.POST("/History/HistoryReport/:id", CreateHistoryReport)
-		apiv1_auth.POST("/History/HistoryReport", CreateHistoryReport)
+
+		// apiv1_auth.POST("/History/HistoryReport", CreateHistoryReport)
+		apiv1_auth.GET("/History/HistoryReport", CreateHistoryReport)
 
 	}
 
