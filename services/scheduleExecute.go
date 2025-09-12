9ed758c49617bdd5263902c5a7443a13f807c436
陳志銓 <chen@Russells-MacBook-Pro.local>
2025-09-12 18:45:26 +0800

    update

--- services/scheduleExecute.go
+++ services/scheduleExecute.go
@@ -113,7 +113,6 @@ func FuncAddToCron(test bool, scheduleID int) {
 	if test {
 		log.Logrecord("排程", fmt.Sprintf("報表試寄完成, Schedule ID : %d", scheduleID))
 	}
-	
 
 	DeleteOldHistory()
 }
@@ -171,6 +170,65 @@ func CreateHistoryReport(historyID int) {
 		}
 
 	}
+	// time_mail := time.Now().Format("2006-01-02 15:04:05")
+	//寄送時間
+	time_mail := time.Now().Unix()
+	history.EmailTime = time_mail
+
+	err = global.Mysql.Create(&history).Error
+	if err != nil {
+		msg := fmt.Sprintf("create history error: %s", err.Error())
+		log.Logrecord("ERROR", msg)
+	}
+
+}
+
+// report 重寄by 不截圖
+func CreateHistoryReports(historyID int) {
+
+	historydata, err := GetHistoryByHistoryID(historyID)
+	if err != nil {
+		log.Logrecord("ERROR", "Get History by History Id error"+err.Error())
+		// fmt.Println(err)
+	}
+
+	inventory, err := GetScheduleBySheduleID(historydata.ScheduleID)
+	if err != nil {
+		log.Logrecord("ERROR", "Get Schedule by Schedule Id error"+err.Error())
+		// fmt.Println(err)
+
+	}
+
+	// 以歷史執行時間為新的執行時間
+	history_ExecuteTime := historydata.ExecuteTime
+
+	// 新的歷史執行時間
+	time_execute := time.Now().Unix()
+	fmt.Println("排程執行時間: ", time_execute)
+	history := entities.History{}
+	history.ScheduleID = historydata.ScheduleID
+	history.To = inventory.To
+	history.BCC = inventory.BCC
+	history.CC = inventory.CC
+	history.ScheduleName = inventory.Name
+	history.ExecuteTime = time_execute
+
+	log.Logrecord("排程", "schedule name: "+inventory.Name)
+	history.Success = "成功"
+
+	err = CreatePDFbySchedule(history_ExecuteTime, historydata.ScheduleID)
+	if err != nil {
+		fmt.Println("產生pdf錯誤", err)
+		history.Success = "產生pdf錯誤"
+	} else {
+		err = SendEmailBySchedule(history_ExecuteTime, historydata.ScheduleID)
+		if err != nil {
+			fmt.Println("發送mail錯誤", err)
+			history.Success = "發送mail錯誤"
+		}
+
+	}
+
 	// time_mail := time.Now().Format("2006-01-02 15:04:05")
 	//寄送時間
 	time_mail := time.Now().Unix()
